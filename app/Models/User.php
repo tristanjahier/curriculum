90149169675e55338228a379141194b1fb39cc77
Tristan Jahier <tristan.jahier@gmail.com>
2026-08-21 11:18:28 +0200

    chore: swap application skeleton for blank Vue starter kit
    The full-fledged starter kit is overwhelming for a Vue learning project.

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,15 +17,12 @@ use Illuminate\Support\Carbon;
  * @property string $email
  * @property Carbon|null $email_verified_at
  * @property string $password
- * @property string|null $two_factor_secret
- * @property string|null $two_factor_recovery_codes
- * @property Carbon|null $two_factor_confirmed_at
  * @property string|null $remember_token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
 #[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
